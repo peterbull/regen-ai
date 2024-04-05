1c1da1e18381dd923dc238834e5fc49ba1a7c02f
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2024-04-05 11:23:55 -0400

    model building correctly and working in docker container

--- regen-requester/entrypoint.sh
+++ regen-requester/entrypoint.sh
@@ -6,10 +6,16 @@ git config --global credential.helper store
 # Login to Hugging Face
 huggingface-cli login --token $HUGGINGFACE_TOKEN --add-to-git-credential
 
-# Download Model
-if [ ! -d "/usr/share/downloads/$HUGGINGFACE_MODEL_NAME" ]; then
-    huggingface-cli download $HUGGINGFACE_MODEL_DIR $HUGGINGFACE_MODEL_NAME --local-dir /usr/share/downloads   --local-dir-use-symlinks False
-fi
+# Set HF_HOME to the mapped volume to prevent extra downloads during development
+export HF_HOME=/usr/share/downloads
+
+# # Download Model
+# if [ ! -d "/usr/share/downloads/$HUGGINGFACE_MODEL_NAME" ]; then
+#     huggingface-cli download $HUGGINGFACE_MODEL_DIR $HUGGINGFACE_MODEL_NAME --local-dir /usr/share/downloads   --local-dir-use-symlinks False
+# fi
+
+# Config model from Modelfile
+# ollama create $OLLAMA_MODEL_ALIAS -f /usr/share/modelfiles/Modelfile
 
 # Start server
 ollama serve
