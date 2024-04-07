4faa895c7186c6f743c0de92c46c1bb8b1a1f1db
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2024-04-07 12:02:07 -0400

    add docstrings

--- backend/app/main.py
+++ backend/app/main.py
@@ -26,16 +26,37 @@ async def root():
     return {"Hello": "Claudio", "Hi There": "Rosie"}
 
 
-@app.get("/weather")
+@app.get("/weather/lostandfound")
 async def weather():
+    """
+    Retrieves the weather forecast
+
+    Returns:
+        dict: A dictionary containing the weather information.
+            - location (str): The location of the lost and found.
+            - temperature (float): The temperature in Fahrenheit.
+            - precipitation (float): The precipitation in inches.
+    """
     return {"location": "San Diego", "temperature": 72.1, "precipitation": 0}
 
 
 @app.get("/air_quality")
 async def air_quality():
+    """
+    Retrieves the air quality information
+
+    Returns:
+        dict: A dictionary containing the location, AQI (Air Quality Index), and pollutant information.
+    """
     return {"location": "San Diego", "AQI": 45, "pollutant": "PM2.5"}
 
 
 @app.get("/traffic")
 async def traffic():
+    """
+    Get traffic information.
+
+    Returns:
+        dict: A dictionary containing traffic information including location, congestion, and average speed.
+    """
     return {"location": "San Diego", "congestion": "Moderate", "average_speed": 45.2}
