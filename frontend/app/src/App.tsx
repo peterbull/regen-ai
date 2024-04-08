9a9c82f5c028ce6ff4f74c1ac613668bfa720043
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2024-04-08 06:26:04 -0400

    scaffold react-ts-tailwind frontend

---
+++ frontend/app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline text-red-600">
+        Simple React Typescript Tailwind Sample
+      </h1>
+    </div>
+  );
+}
+
+export default App;
