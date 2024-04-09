3a27c6b59f5de50a73c41211ab30110939356fcf
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2024-04-09 05:48:25 -0400

    ensure s3 fetch only occurs on backend change

--- frontend/app/src/components/Weather.tsx
+++ frontend/app/src/components/Weather.tsx
@@ -25,44 +25,39 @@ const Weather: React.FC<any> = () => {
           Bucket: process.env.REACT_APP_S3_BUCKET_NAME,
           Key: "endpoints.json",
         };
-        s3.headObject(params, function (err, data: any) {
-          if (err) console.log(err, err.stack); // an error occurred
-          else {
-            const lastModified = new Date(data.LastModified);
-            if (lastModified > lastUpdated) {
-              setLastUpdated(lastModified);
-              s3.getObject(params, function (err, data: any) {
-                if (err) console.log(err, err.stack);
-                else {
-                  let endpoint = JSON.parse(data.Body.toString());
-                  endpoint.weather = endpoint.weather.replace(
-                    "backend",
-                    "localhost"
-                  );
-                  setWeatherEndpoint(endpoint.weather);
-                  console.log(endpoint.weather);
-                }
-              });
-            }
-          }
-        });
-      } catch (error) {
-        console.error(error);
+
+        const data: any = await s3.headObject(params).promise();
+        const lastModified = new Date(data.LastModified);
+        if (lastModified > lastUpdated) {
+          setLastUpdated(lastModified);
+          const new_data: any = await s3.getObject(params).promise();
+          let endpoint = JSON.parse(new_data.Body.toString());
+          endpoint.weather = endpoint.weather.replace("backend", "localhost");
+          setWeatherEndpoint(endpoint.weather);
+          console.log(endpoint.weather);
+        }
+      } catch (err: any) {
+        console.log(err, err.stack);
       }
     };
     fetchEndpoint();
-    const intervalId = setInterval(fetchEndpoint, 10000);
-    return () => clearInterval(intervalId); // Clear interval on component unmount
-  }, [lastUpdated]);
+  }, [weather, lastUpdated]);
 
   useEffect(() => {
     const fetchWeather = async () => {
       const res = await fetch(weatherEndpoint);
-      const data = await res.json();
-      setWeather(data);
-      console.log(data);
+      if (res.status !== 200) {
+        setWeather([{ weather: "Endpoint Changed" }]);
+      } else {
+        const data = await res.json();
+        setWeather(data);
+      }
+      console.log(weatherEndpoint);
     };
     fetchWeather();
+
+    const intervalId = setInterval(fetchWeather, 10000);
+    return () => clearInterval(intervalId); // Clear interval on component unmount
   }, [weatherEndpoint]);
 
   return (
