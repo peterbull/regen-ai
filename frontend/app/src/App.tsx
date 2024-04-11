4b33c9c9bd410ff4c3aa143d6627ed1a95078e48
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2024-04-11 07:08:52 -0400

    add nav to tracing backend

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -9,6 +9,16 @@ function App() {
         Regen APAI
       </h1>
       <Weather />
+
+      <button className="inline-block font-extralight text-2xl text-custom-blue py-3 border-2 border-custom-blue rounded-xl p-4 mt-6 uppercase hover:bg-custom-blue hover:text-custom-light-green active:bg-custom-teal active:text-custom-off-white">
+        <a
+          href="http://localhost:6006"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Trace in Phoenix
+        </a>
+      </button>
     </div>
   );
 }
